8795da95930e2b13972be5f5fd62995291e89eb0
mahdihp.devsc <messenger123456>
2024-02-02 13:03:06 +0330

    Git Init 1402-11-13 IsUsernameUnique

---
+++ repository/postgres/postgresuser/db.go
@@ -0,0 +1,33 @@
+package postgresuser
+
+import (
+	"context"
+	"entgo.io/ent/dialect/sql"
+	"siliconvali/ent/user"
+	richerror "siliconvali/pkg"
+	"siliconvali/pkg/errmsg"
+	"siliconvali/repository/postgres"
+)
+
+type DB struct {
+	conn *postgres.PostgresqlDB
+}
+
+func New(conn *postgres.PostgresqlDB) *DB {
+	return &DB{
+		conn: conn,
+	}
+}
+
+func (d *DB) IsUsernameUnique(phoneNumber string) (bool, error) {
+	const op = "postgres.IsUsernameUnique"
+	ctx := context.Background()
+
+	found, err := d.conn.Conn().User.Query().Where(sql.FieldEQ(user.FieldUsername, phoneNumber)).Exist(ctx)
+	if err != nil {
+		return false, richerror.New(op).WithErr(err).
+			WithMessage(errmsg.ErrorMsgCantScanQueryResult).WithKind(richerror.KindUnexpected)
+	}
+
+	return found, nil
+}
