dc9dbd80d1fb63258dfde7669751fd0f7cef11a9
mahdihp.devsc <messenger123456>
2024-02-08 18:38:36 +0330

    Add & Update Repositories 3

--- services/userservice/user_service.go
+++ services/userservice/user_service.go
@@ -5,12 +5,30 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"siliconvali/dto"
+	"siliconvali/ent"
 )
 
-type UserService interface {
+type UserServiceRepositort interface {
 	Register(u dto.UserInsertRequest) (dto.UserInsertResponse, error)
 	GetUserByPhoneNumber(phoneNumber string) (dto.UserInfo, error)
 	GetUserByID(ctx context.Context, userId uint) (dto.UserInfo, error)
+}
+
+type AuthGenerator interface {
+	CreateAccessToken(user ent.User) (string, error)
+	CreateRefreshToken(user ent.User) (string, error)
+}
+
+type UserServiceRepositortImpl struct {
+	auth AuthGenerator
+	repo UserServiceRepositort
+}
+
+func New(authGenerator AuthGenerator, repo UserServiceRepositort) UserServiceRepositortImpl {
+	return UserServiceRepositortImpl{
+		auth: authGenerator,
+		repo: repo,
+	}
 }
 
 func getMD5Hash(text string) string {
